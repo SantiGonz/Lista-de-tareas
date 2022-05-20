75263596bb24d40aa2f077d46d62e5d2ad436da0
Santiago Gonzalez <321090412@alumno.enp.unam.mx>
2022-05-20 17:42:05 -0500

    logré el input adicional y el despliegue del div de tareas

--- dynamics/lista.js
+++ dynamics/lista.js
@@ -1,7 +1,29 @@
 const botoncito = document.getElementById("botoncito");
 const input = document.getElementById("input");
-const otra = document.getElementById("otra")
+const otra = document.getElementById("otra");
+const cuadrito = document.getElementById("cuadrito");
+const materia = document.getElementById("materia");
+const despliegue = document.getElementById("despliegue");
+const select = document.getElementById("select");
 
-otra.addEventListener("click", () =>{
-    otra.innerHTML += "<input type='text' id='otraa'>";
+var tareasTot = 0;
+var tareasCom = 0;
+
+materia.addEventListener("click", (evento) =>{
+    console.log("si funciona");
+    if(materia.value==2){
+    select.innerHTML += '<input type="text" name="otra" id="otra">';
+    console.log(materia.id);
+    }
+});
+
+botoncito.addEventListener("click", (evento) =>{
+    // if(input.value!=null){
+        despliegue.style.display = "block";
+        console.log(otra.value);
+        materia.innerHTML += "<option value='1'>" + otra.value + "</option>";
+        tareasTot++;
+
+    //}
+
 })
